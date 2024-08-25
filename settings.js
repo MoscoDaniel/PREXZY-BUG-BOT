a6bb5cb2b2f628607794fe4b75d910a004fd41f4
MoscoDaniel <moscooffixial@gmail.com>
2024-08-25 19:02:38 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "263717750825"
-global.ownername = "Precious Ayomide👑"
+global.ownernomer = "09047183997"
+global.ownername = "Daniel tech"
 global.ytname = "YT: Prexzyvilla"
 global.socialm = "GitHub: Prexzybooster"
 global.location = "Nigeria, Osun, Osogbo"
